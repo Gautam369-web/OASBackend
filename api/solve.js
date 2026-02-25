553513e2f5be22538a4890660e4d312607aacaef
Gautam Kushwaha <gautamkumarkushwaha123456@gmail.com>
2026-02-25 19:38:36 +0530

    Switch to gemma-3-1b-it for better availability

--- api/solve.js
+++ api/solve.js
@@ -105,7 +105,7 @@ Options:
 ${formattedOptions}
 Answer Number:`;
 
-        const geminiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=${geminiKey}`;
+        const geminiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemma-3-1b-it:generateContent?key=${geminiKey}`;
 
         const response = await fetch(geminiUrl, {
             method: "POST",
